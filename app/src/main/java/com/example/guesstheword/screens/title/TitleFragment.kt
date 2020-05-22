e392e75fc19a1b302a0dbda79b8c56635d406484
riskymind <thisiskelchi@gmail.com>
2020-05-22 13:52:21 +0100

    implement the game title fragment

---
+++ app/src/main/java/com/example/guesstheword/screens/title/TitleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.guesstheword.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+import com.example.guesstheword.R
+import com.example.guesstheword.databinding.TitleFragmentBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.title_fragment, container, false)
+
+        return binding.root
+    }
+
+}
