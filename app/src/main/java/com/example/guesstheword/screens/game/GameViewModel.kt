f7280373fc34fccbbe6a0c54ec5f54c5b1501b51
riskymind <thisiskelchi@gmail.com>
2020-05-25 09:45:44 +0100

    added the game timer of 60seconds

--- app/src/main/java/com/example/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/guesstheword/screens/game/GameViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
@@ -18,6 +21,22 @@ class GameViewModel: ViewModel() {
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    private val timer: CountDownTimer
+
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    val wordHint = Transformations.map(word){ word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" + "\nThe letter at positon " +
+                randomPosition + " is " + word[randomPosition - 1].toUpperCase()
+    }
+
     private lateinit var wordList: MutableList<String>
 
     init {
@@ -25,6 +44,19 @@ class GameViewModel: ViewModel() {
         _score.value = 0
         resetList()
         nextWord()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+
+        }
+        timer.start()
     }
 
     private fun resetList() {
@@ -60,5 +92,16 @@ class GameViewModel: ViewModel() {
         _eventGameFinish.value = false
     }
 
+    companion object {
+        private const val DONE = 0L
 
+        private const val ONE_SECOND = 1000L
+
+        private const val COUNTDOWN_TIME = 60000L
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
 }
