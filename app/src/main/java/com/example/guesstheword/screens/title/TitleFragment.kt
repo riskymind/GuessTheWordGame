81d4f340552da63ee97f73f79c91a8100ff94b5a
riskymind <thisiskelchi@gmail.com>
2020-05-25 22:26:48 +0100

    added the about fragment and option menu

--- app/src/main/java/com/example/guesstheword/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/guesstheword/screens/title/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.guesstheword.screens.title
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 
 import com.example.guesstheword.R
 import com.example.guesstheword.databinding.TitleFragmentBinding
@@ -27,7 +27,17 @@ class TitleFragment : Fragment() {
             findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
 
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.option_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
